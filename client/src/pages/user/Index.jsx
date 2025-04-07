3090a452612e892ad97544e41d2897da0431ddab
3ai <100020625+3aiii@users.noreply.github.com>
2025-04-07 14:38:01 +0700

    Refactor code structure for improved readability and maintainability

--- client/src/pages/user/Index.jsx
+++ client/src/pages/user/Index.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 const Index = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="bg-white p-8 rounded-xl shadow-md text-center max-w-sm w-full">
-        <h1>ยินดีต้อนรับสู่เว็บไซต์จำแนกสายพันธุ์สุนัข</h1>
+      <div className="bg-white p-8 rounded-xl shadow-md text-center max-w-lg w-full">
+        <h1 className="text-2xl font-semibold">
+          ยินดีต้อนรับสู่เว็บไซต์จำแนกสายพันธุ์สุนัข
+        </h1>
         <img src="/cute-dog.jpg.png" alt="น้องหมาน่ารัก" />
         <br />
         <Link to="/login" className="btn-green text-white">
